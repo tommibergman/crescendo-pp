9b04e6636f48daa94226ae7c99a1db6df7a7e526
tommibergman <tommi.bergman@iki.fi>
2018-04-12 09:35:39 +0000

    create_jobfiles takes now arguments with simple sanity check

--- create_jobfiles.sh
+++ create_jobfiles.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
+if [[ -n "${1}" ]]
+then
+    y1=${1} 
+else
+    echo "usage ${0} startyear [endyear]"
+    exit
+fi
 
-for yyyy in {2000..2014}
+#if no end
+[[ -n "${2}" ]] && y2=${2} || y2=${1}
+
+if [[ ${y2} -lt ${y1} ]]
+then
+    echo "usage ${0} startyear [endyear]"
+    echo "endyear must be larger than or equal to staryear"
+    exit
+fi
+
+for yyyy in $(eval echo "{$y1..$y2}")
 do
     sed  "s/yyyy/${yyyy}/g" pp_ifs_monthly.job.tmpl > pp_ifs_monthly.${yyyy}.job
     sed  "s/yyyy/${yyyy}/g" pp_ifs+tm5_merge_copy.job.tmpl > pp_ifs+tm5_merge_copy.${yyyy}.job
